4a9709e8c8c6d536865e8a05580de026ce34febe
Robert Chung <robertchung97@gmail.com>
2018-04-27 20:47:43 -0700

    modularized help parsing to a function. Initial startShell() function.

--- falsh.c
+++ falsh.c
@@ -3,31 +3,53 @@
 #include <unistd.h>
 
 // Function protoptypes
-void handleHelp();
+void handleHelp(int, char *argv[]);
+void startShell();
 
 
 int main (int argc, char *argv[])
 {
-	int opt;
 
+	handleHelp(argc, argv);
+	startShell();
+
+
+
+	
+	return 0;
+}
+
+// handleHelp uses getopt to parse command line options
+// if -h is used, prints out a help message and exits.
+// Only happens during falsh invocation
+void handleHelp(int argc, char *argv[])
+{
+	int opt;		// variable needed for getopt
+
+	// getopt gets the next option argument from argc and argv
+	// "h" is to indicate that -h is a valid argument
+	// returns -1 when nothing else is to be read
 	while ((opt = getopt(argc, argv, "h")) != -1)
 	{
 		switch(opt)
 		{
+			// Help message if correctly invoked
 			case 'h':
-				handleHelp();
+				printf("THIS IS A HELP MESSAGE\n");
+				exit(0);
 				break;
-			default:
-				fprintf(stderr, "Unknown arugment");
+			// Any illegal actions causes error message and exit
+			case '?':
+				exit(EXIT_FAILURE);
 		}
 	}
-	return 0;
+
 }
 
-// handleHelp prints out a help message and exits.
-// Only happens during falsh invocation
-void handleHelp()
+void startShell()
 {
-	printf("THIS IS A HELP MESSAGE\n");
-	exit(EXIT_FAILURE);
+	while (1)
+	{
+
+	}
 }
